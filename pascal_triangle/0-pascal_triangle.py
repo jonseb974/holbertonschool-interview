be6b1765dae0582a9e0942d479a0f387d7ac9bf0
jonseb974 <5151@holbertonstudents.com>
2023-03-25 16:40:40 +0100

    add scritp file0_pascal

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of 'lists of integers' representing
+    the Pascal’s triangle of n:
+        Returns an empty list if n <= 0
+        You can assume n will be always an integer
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j-1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+    return triangle
