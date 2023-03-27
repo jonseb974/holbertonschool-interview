ab28ce69fd7293f85e8593762f9cbaf9e4f4a0c1
jonseb974 <5151@holbertonstudents.com>
2023-03-27 14:11:52 +0200

    add script test

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 
 
-def canUnlockAll(boxes)
+def canUnlockAll(boxes):
+    # Keep track of which boxes we've seen and whether they're open or not
+    seen = [False] * len(boxes)
+    seen[0] = True
+    open_boxes = [0]
+
+    # Perform a depth-first search starting from box 0
+    while open_boxes:
+        box = open_boxes.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and not seen[key]:
+                seen[key] = True
+                open_boxes.append(key)
+
+    # If we've seen all the boxes, return True
+    return all(seen)
