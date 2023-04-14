44664a63f1209b9af6b66a92c6f7176ba5632c19
jonseb974 <5151@holbertonstudents.com>
2023-04-14 16:50:10 +0200

    add 0-statsfile

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -7,7 +7,7 @@
 import sys
 from collections import defaultdict
 
-# 
+
 def parse_line(line):
     """
     Define a function to parse the input line
@@ -27,15 +27,19 @@ def parse_line(line):
 
 # Initialize variables to keep track of the total file size and
 # the number of lines by status code
+
+
 total_size = 0
 lines_by_status = defaultdict(int)
 num_lines = 0
+
 
 # Define a function to print the statistics
 def print_statistics():
     print(f"Total file size: {total_size}")
     for status_code in sorted(lines_by_status.keys()):
         print(f"{status_code}: {lines_by_status[status_code]}")
+
 
 # Loop over stdin, reading one line at a time
 try:
@@ -45,16 +49,16 @@ try:
         if parsed is None:
             continue
         ip_address, status_code, file_size = parsed
-        
+
         # Update the total file size and the number of lines by status code
         total_size += file_size
         lines_by_status[status_code] += 1
         num_lines += 1
-        
+
         # Print statistics every 10 lines
         if num_lines % 10 == 0:
             print_statistics()
-            
+
 except KeyboardInterrupt:
     # If the user interrupts the script, print the final statistics
     print_statistics()
