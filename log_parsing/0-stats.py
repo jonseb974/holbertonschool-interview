6ba403347a2225c653eed127a171635a67c90c87
jonseb974 <5151@holbertonstudents.com>
2023-04-14 17:15:59 +0200

    add new script 0-stats

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,66 +1,69 @@
 #!/usr/bin/python3
-"""
+""""
 0-log_parsing
 """
 
 
 import sys
-from collections import defaultdict
 
 
-def parse_line(line):
+def extract_status_code(line):
     """
-    Define a function to parse the input line
-    and return the IP address,
-    status code, and file size
+    Define a function to extract the status code from a log line
     """
-    parts = line.split()
-    if len(parts) != 7:
+    fields = line.split()
+    if len(fields) < 8:
         return None
-    ip_address, _, _, timestamp, _, status_code, file_size = parts
     try:
-        status_code = int(status_code)
-        file_size = int(file_size)
+        status_code = int(fields[7])
+        return status_code
     except ValueError:
         return None
-    return ip_address, status_code, file_size
 
-# Initialize variables to keep track of the total file size and
-# the number of lines by status code
+# Initialize variables to hold the statistics
 
 
 total_size = 0
-lines_by_status = defaultdict(int)
-num_lines = 0
+status_counts = {
+    200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+    }
+line_count = 0
 
+# Read lines from standard input
+for line in sys.stdin:
+    line_count += 1
 
-def print_statistics():
-    """
-    Define a function to print the statistics
-    """
-    print(f"Total file size: {total_size}")
-    for status_code in sorted(lines_by_status.keys()):
-        print(f"{status_code}: {lines_by_status[status_code]}")
+    # Extract the file size from the log line
+    fields = line.split()
+    if len(fields) < 8:
+        continue
+    try:
+        file_size = int(fields[8])
+    except ValueError:
+        continue
 
+    # Extract the status code from the log line and update the status count
+    status_code = extract_status_code(line)
+    if status_code is not None and status_code in status_counts:
+        status_counts[status_code] += 1
 
-# Loop over stdin, reading one line at a time
-try:
-    for line in sys.stdin:
-        # Parse the input line
-        parsed = parse_line(line.strip())
-        if parsed is None:
-            continue
-        ip_address, status_code, file_size = parsed
+    # Update the total file size
+    total_size += file_size
 
-        # Update the total file size and the number of lines by status code
-        total_size += file_size
-        lines_by_status[status_code] += 1
-        num_lines += 1
+    # If we've reached the end of a block of 10 lines,
+    # or if we've received a keyboard interrupt, print the statistics
+    if line_count % 10 == 0:
+        print("File size:", total_size)
+        for status_code in sorted(status_counts.keys()):
+            count = status_counts[status_code]
+            if count > 0:
+                print("{}: {}".format(status_code, count))
+        print()
+        sys.stdout.flush()
 
-        # Print statistics every 10 lines
-        if num_lines % 10 == 0:
-            print_statistics()
-
-except KeyboardInterrupt:
-    # If the user interrupts the script, print the final statistics
-    print_statistics()
+print("File size:", total_size)
+for status_code in sorted(status_counts.keys()):
+    count = status_counts[status_code]
+    if count > 0:
+        print("{}: {}".format(status_code, count))
+sys.stdout.flush()
