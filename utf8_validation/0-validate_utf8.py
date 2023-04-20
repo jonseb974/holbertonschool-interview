8416b7bacdfb9f06a452de798c2f9f6cdc26dfc7
jonseb974 <5151@holbertonstudents.com>
2023-04-20 09:43:23 +0200

    add file 0-validate_utf8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -2,3 +2,7 @@
 """
 Method that determines if a given data set represents a valid UTF-8 encoding.
 """
+
+
+def validUTF8(data):
+    """Return: True if data is a valid UTF-8 encoding, else return False"""
