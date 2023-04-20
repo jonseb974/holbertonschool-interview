308647f98f89f215d847ae2e4dc0c1e0b2c232cc
jonseb974 <5151@holbertonstudents.com>
2023-04-20 11:54:53 +0200

    add file 0-validate_utf8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -5,4 +5,32 @@ Method that determines if a given data set represents a valid UTF-8 encoding.
 
 
 def validUTF8(data):
-    """Return: True if data is a valid UTF-8 encoding, else return False"""
+    """
+    Return: True if data is a valid UTF-8 encoding, else return False
+    """
+
+    data_bytes = 0
+
+    for data_byte in data:
+        binary_str = format(data_byte, '08b')[-8:]
+
+        if data_bytes == 0:
+            if binary_str[0] == '0':  # Begining of new UTF-8 character
+                continue  # single-bite character keep analysing the data.
+
+            elif binary_str.startswith('110'):  # this is a two byte character
+                data_bytes = 1
+
+            elif binary_str.startswith('1110'):  # Three byte character.
+                data_bytes = 2
+
+            elif binary_str.startswith('11110'):  # Four byte character.
+                data_bytes = 3
+            else:
+                return False  # Invalide sequence of byte.
+        else:
+            if binary_str.startswith('10'):  # Start in the middle of UTF-8
+                data_bytes -= 1
+            else:
+                return False  # Invalid sequence of bytes
+    return data_bytes == 0
