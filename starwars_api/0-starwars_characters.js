8a6cb12c131609549edc25ea3f281b73ae512d08
jonseb974 <5151@holbertonstudents.com>
2023-05-05 22:32:47 +0200

    add 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,44 +2,20 @@
 
 const request = require('request');
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-// Make a GET request to the Star Wars API to get the movie details
-request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
+request.get(url, async (error, response, body) => {
+  if (error) console.log(error.message);
+  const film = JSON.parse(body);
 
-  if (response.statusCode !== 200) {
-    console.error(`Error: Received status code ${response.statusCode}`);
-    return;
-  }
-
-  const movie = JSON.parse(body);
-
-  let characterCount = 0;
-  movie.characters.forEach((characterUrl, index) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error(`Error: Received status code ${response.statusCode}`);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      characterCount++;
-      console.log(`${characterCount}. ${character.name}`);
-
-      if (characterCount === movie.characters.length) {
-        // All characters have been retrieved, exit the program
-        process.exit(0);
-      }
+  for (const character of film.characters) {
+    await new Promise((resolve) => {
+      request.get(character, (error, response, body) => {
+        if (error) console.log(error.message);
+        const character = JSON.parse(body);
+        console.log(character.name);
+        resolve();
+      });
     });
-  });
+  }
 });
