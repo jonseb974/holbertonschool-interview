59ef77fdd614f484c19b227530aebee1f7030fe8
jonseb974 <5151@holbertonstudents.com>
2023-05-05 10:44:41 +0200

    add 0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -3,29 +3,23 @@
 const request = require('request');
 const movieId = process.argv[2];
 
-request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+request(`https://swapi.dev/api/films/${movieId}/`, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
 
-  const movie = JSON.parse(body);
-  const characterUrls = movie.characters;
-  const characterNames = [];
+  const characters = JSON.parse(body).characters;
 
-  characterUrls.forEach((url) => {
-    request(url, (error, response, body) => {
+  characters.forEach(function (characterUrl) {
+    request(characterUrl, function (error, response, body) {
       if (error) {
         console.error(error);
         return;
       }
 
-      const character = JSON.parse(body);
-      characterNames.push(character.name);
-
-      if (characterNames.length === characterUrls.length) {
-        characterNames.forEach((name) => console.log(name));
-      }
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
     });
   });
 });
