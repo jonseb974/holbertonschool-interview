09e6897393277f69f9e13a6f4ee55a936a85fa90
jonseb974 <5151@holbertonstudents.com>
2023-05-11 16:08:40 +0200

    pycodestyle update

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,13 +5,13 @@ Test  - Rotate 2D Matrix
 
 
 def rotate_2d_matrix(matrix):
-    """Rotate the list of given numbers 
+    """Rotate the list of given numbers
     """
     n = len(matrix)  # define the matrix store this value in a variable
 
     for i in range(n):
-        for j in range (i, n):
+        for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     for i in range(n):
         matrix[i] = matrix[i][::-1]
